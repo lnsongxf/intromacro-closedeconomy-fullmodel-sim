41706ff063693dd0bfca2bacfba2edbf5f89ac7a
Christopher Cotton <cdc2175@columbia.edu>
2017-02-28 01:28:59 -0500

    'Changed python preamble'

--- example_increaseM.py
+++ example_increaseM.py
@@ -4,6 +4,8 @@
 # Christopher David Cotton (c)
 # http://www.cdcotton.com
 
+# modules needed for preamble
+import importlib
 import os
 import sys
 
@@ -25,18 +27,28 @@ def getprojectdir(filename):
 # Directory of project
 __projectdir__ = getprojectdir(__fullrealfile__)
 
-# Setting up importattr (function to load attributes from files by absolute path)
-if 'fullmodulesloaded' not in globals():
-    fullmodulesloaded = {}
-sys.path.append(__projectdir__ + 'submodules/python-preamble/')
-from importattr2 import importattr2
-
-def importattr(filename, function, fullmodulesloaded = fullmodulesloaded, curfilename = __fullrealfile__, bybasename = False):
-    if os.path.abspath(filename) == curfilename:
-        func = eval(function)
+# Function to call functions from files by their absolute path.
+# Imports modules if they've not already been imported
+# First argument is filename, second is function name, third is dictionary containing loaded modules.
+modulesdict = {}
+def importattr(modulefilename, func, modulesdict = modulesdict):
+    # if function in this file
+    if modulefilename == __fullrealfile__:
+        return(eval(func))
     else:
-        func = importattr2(filename, function, fullmodulesloaded, bybasename = bybasename)
-    return(func)
+        # add file to moduledict if not there already
+        if modulefilename not in modulesdict:
+            # check filename exists
+            if not os.path.isfile(modulefilename):
+                print('ERROR: ' + modulefilename + ' does not exist.')
+                sys.exit(1)
+            # add directory to path
+            sys.path.append(os.path.dirname(modulefilename))
+            # actually add module to moduledict
+            modulesdict[modulefilename] = importlib.import_module(''.join(os.path.basename(modulefilename).split('.')[: -1]))
+
+        # get the actual function from the file and return it
+        return(getattr(modulesdict[modulefilename], func))
 
 # PYTHON_PREAMBLE_END:}}}
 
